4d58d5c7af309757b2d45e1a352f8596b86a35e4
Mark X <khozinmark@yandex.ru>
2022-11-23 22:09:18 +0300

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect} from 'react';
 import DrumMachine from '../features/drum-machine/DrumMachine';
 import {loadAudioBuffers} from '../features/drum-machine/drumMachineSlice';
@@ -5,9 +6,12 @@ import {useAppDispatch, useAppSelector} from './hooks';
 
 export default function App(): JSX.Element {
   const dispatch = useAppDispatch();
+
   const {audioContext} = useAppSelector(state => state.drumMachine);
+
   useEffect(() => {
     dispatch(loadAudioBuffers(audioContext));
   }, []);
+
   return <DrumMachine />;
 }
